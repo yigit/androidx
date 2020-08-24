016f7825a28d76d42a404afe572a8dfc63c75935
Yigit Boyar <yboyar@google.com>
2020-08-23 23:01:19 -0700

    Remove primitive-specific calls from XType
    This CL does a cleanup in XType for easier KSP support.
    
    * Removed `isPrimitiveXXX`  methods from XType. We don't really
    care if something is a primitive, it was only used as a proxy
    for nullability.
    
    * Moved `isInt, isLong, isByte`  into implementations of XType
    instead since they are all 1 class in kotlin but two types in Java.
    
    * Removed `isNot`  methods and instead moved them into an extension
    file in Room.
    
    Bug: 160322705
    Test: existing tests  + a new Long[] test case
    Change-Id: I583cc5fa970f236eb6a22026303104b2ca847805

--- room/compiler-processing/src/test/java/androidx/room/compiler/processing/XExecutableElementTest.kt
+++ room/compiler-processing/src/test/java/androidx/room/compiler/processing/XExecutableElementTest.kt
@@ -22,6 +22,7 @@ import androidx.room.compiler.processing.util.getMethod
 import androidx.room.compiler.processing.util.getParameter
 import androidx.room.compiler.processing.util.runProcessorTest
 import com.google.common.truth.Truth.assertThat
+import com.squareup.javapoet.ClassName
 import com.squareup.javapoet.TypeName
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -38,7 +39,7 @@ class XExecutableElementTest {
                 package foo.bar;
                 public class Baz {
                     private void foo() {}
-                    public int bar(int param1) {
+                    public String bar(String[] param1) {
                         return 3;
                     }
                 }
@@ -59,10 +60,12 @@ class XExecutableElementTest {
             element.getDeclaredMethod("bar").let { method ->
                 assertThat(method.isOverrideableIgnoringContainer()).isTrue()
                 assertThat(method.parameters).hasSize(1)
+                val stringTypeName = ClassName.get("java.lang", "String")
                 method.getParameter("param1").let { param ->
-                    assertThat(param.type.isPrimitiveInt()).isTrue()
+                    assertThat(param.type.isArray()).isTrue()
+                    assertThat(param.type.asArray().componentType).isEqualTo(stringTypeName)
                 }
-                assertThat(method.returnType.isPrimitiveInt()).isTrue()
+                assertThat(method.returnType.typeName).isEqualTo(stringTypeName)
             }
         }
     }
